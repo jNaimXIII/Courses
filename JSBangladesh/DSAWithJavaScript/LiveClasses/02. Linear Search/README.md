644af622edcc075d898bda8f7efd1e88e55d41ed
Jannatin Naim <soup@jannatinnaim.com>
2023-03-04 16:55:37 +0600

    DSA With JavaScript - Linear Search Class

---
+++ JSBangladesh/DSAWithJavaScript/LiveClasses/02. Linear Search/README.md
@@ -0,0 +1,3 @@
+# Linear Search
+
+[Notion](https://jnaimxiii.notion.site/02-Linear-Search-06e7be65b4bf41c3a861295c4a34bd17)
---
+++ JSBangladesh/DSAWithJavaScript/LiveClasses/02. Linear Search/linearSort.js
@@ -0,0 +1,27 @@
+/**
+ * search through an array for a target value
+ * @param {any[]} arr
+ * @param {any} target
+ * @returns {number} index of target if found, otherwise -1
+ */
+function linearSearch(arr, target) {
+  // iterate over array
+  for (let currentIndex = 0; currentIndex < arr.length; ++currentIndex) {
+    const currentElement = arr[currentIndex];
+
+    if (currentElement === target) {
+      // early return with index of target
+      return currentIndex;
+    }
+  }
+
+  // explicitly return -1 if target is not found
+  return -1;
+}
+
+// unordered array
+const arr = [10, 20, 5, 15, 25, 45, 100];
+
+// examples
+console.log(linearSearch(arr, 15));
+console.log(linearSearch(arr, 0));
