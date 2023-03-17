677be3a22ec09ed3e40a4dc42f5e6af9056a076e
Jannatin Naim <soup@jannatinnaim.com>
2023-03-17 15:12:51 +0600

    DSA With JavaScript - Complexity Analysis

--- JSBangladesh/DSAWithJavaScript/LiveClasses/04. Bubble Sort & Selection Sort/bubbleSort.js
+++ JSBangladesh/DSAWithJavaScript/LiveClasses/04. Bubble Sort & Selection Sort/bubbleSort.js
@@ -3,6 +3,7 @@
  * @param {number[]} arr
  */
 function bubbleSort(arr) {
+  let iterations = 0;
   // outer loop iterating over every item
   for (let i = 0; i < arr.length; ++i) {
     // inner loop iterating over next items of outer loop's item
@@ -16,8 +17,11 @@ function bubbleSort(arr) {
         // fancier version of swapping in js without introducing a variable
         // [arr[i], arr[j]] = [arr[j], arr[i]]
       }
+      iterations++;
     }
+    iterations++;
   }
+  console.log(iterations);
 }
 
 // unordered array
@@ -26,3 +30,29 @@ let arr = [10, 5, 12, 7, 8, 3, 100, 60, 5, 10, 8, 7];
 // examples
 bubbleSort(arr);
 console.log(arr);
+
+// // NOTE: this sorts from the end of the array instead of the beginning
+// /**
+//  * sort an array using bubble sorting algorithm (ascending)
+//  * @param {number[]} arr
+//  */
+// function bubbleSort(arr) {
+//   let iterations = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length - 1 - i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//       iterations++;
+//     }
+//     iterations++;
+//   }
+//   console.log(iterations);
+// }
+//
+// // unordered array
+// let arr = [10, 5, 12, 7, 8, 3, 100, 60, 5, 10, 8, 7];
+//
+// // examples
+// bubbleSort(arr);
+// console.log(arr);
---
+++ JSBangladesh/DSAWithJavaScript/LiveClasses/05. Complexity Analysis/README.md
@@ -0,0 +1,3 @@
+# Complexity Analysis
+
+[Notion](https://jnaimxiii.notion.site/05-Complexity-Analysis-d1050774ded042a5b71896721398efa2)
